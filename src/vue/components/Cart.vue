9759baa2e6f5227dced330cd2b2cdad5f344ee58
Dina Hrist <hdina@inbox.ru>
2020-07-15 01:53:37 +0700

    Start header desktop

--- src/vue/components/Cart.vue
+++ src/vue/components/Cart.vue
@@ -2,13 +2,28 @@
 import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
+    data: () => ({
+        loaded: false
+    }),
     computed: {
         ...mapState('cart', ['products']),
         ...mapGetters('cart', ['cnt', 'total']),
         ...mapState('favorite', ['favorites']),
         ...mapGetters('favorite', {
             cntFavorites: 'cnt'
-        })
+        }),
+        hasInCart() {
+            return this.cnt > 0
+        },
+        classHasInCart() {
+            return { 'has-count': this.hasInCart }
+        },
+        classHasFavorites() {
+            return { 'has-count': this.cntFavorites > 0 }
+        },
+        setLoaded() {
+            return { loaded: this.loaded }
+        }
     },
     methods: {
         ...mapActions('products', {
@@ -18,6 +33,9 @@ export default {
     created() {
         // Запрос за детальными данными для корзины
         this.getItems({ ids: this.products.map(item => item.id) })
+    },
+    mounted() {
+        this.loaded = true
     }
 }
 </script>

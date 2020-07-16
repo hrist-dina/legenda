1a735b851b3d67246ef53d6ec505c6d9240cf6ee
Dina Hrist <hdina@inbox.ru>
2020-07-17 03:41:20 +0700

    Some fix location logic

--- src/vue/components/Location.vue
+++ src/vue/components/Location.vue
@@ -12,10 +12,25 @@ export default {
             this.active = !this.active
         },
         onItem({ target }) {
-            new HTTP(urlAjax.location, { code: target.dataset.code }, () => {
+            const codeItem = target.dataset.code
+            const title = this.$refs['title']
+            if (title.dataset.code === codeItem) {
+                this.active = false
+                return
+            }
+            new HTTP(urlAjax.location, { code: codeItem }, () => {
                 this.$refs['title'].textContent = target.textContent
+                this.active = false
             }).post()
         }
+    },
+    mounted() {
+        document.addEventListener('click', ({ target }) => {
+            const el = target.closest(`.${this.$el.classList[0]}`)
+            if ((this.active && !el) || this.$el !== el) {
+                this.active = false
+            }
+        })
     }
 }
 </script>

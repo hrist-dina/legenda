a028565f6f05315c13307e7fdeb46f981dfe24eb
Dina Hrist <hdina@inbox.ru>
2020-08-05 00:04:25 +0700

    Added initial state user data

--- src/vue/store/init.js
+++ src/vue/store/init.js
@@ -7,7 +7,22 @@ export default class Init {
     }
 
     process() {
+        this.user()
         this.cites()
+    }
+
+    user() {
+        const user = this.data.user
+        if (user && user.id) {
+            this.store.commit('user/setId', user.id)
+        }
+        if (user && user.token) {
+            this.store.commit('user/setToken', user.token)
+        }
+
+        if (user && user.person) {
+            this.store.commit('user/setPersonalData', this.data.user.person)
+        }
     }
 
     cites() {

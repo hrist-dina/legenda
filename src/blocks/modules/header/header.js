268a23a4df255b62f3637223ae48797fec65cc25
Dina Hrist <hdina@inbox.ru>
2020-09-16 20:31:30 +0700

    Added search mobile

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -6,6 +6,11 @@ export default class Header extends ComponentBase {
     }
 
     init() {
+        this.onMenuOpen()
+        this.onSearchMobile()
+    }
+
+    onMenuOpen() {
         const header = document.getElementById(this.selector)
         const headerMenuOpen = document.getElementById(
             `${this.selector}-menu-open`
@@ -15,6 +20,30 @@ export default class Header extends ComponentBase {
         headerMenuOpen.addEventListener('click', () => {
             header.classList.toggle('menu-opened')
             document.querySelector('body').classList.toggle('o-hidden')
+        })
+    }
+
+    onSearchMobile() {
+        const headerSearchOpen = document.getElementById(
+            `${this.selector}-search-open`
+        )
+
+        const headerSearchMobile = document.getElementById(
+            `${this.selector}-search-mobile`
+        )
+        if (!headerSearchOpen && !headerSearchMobile) return
+        headerSearchOpen.addEventListener('click', () => {
+            headerSearchMobile.classList.add('active')
+            document.querySelector('body').classList.add('o-hidden')
+        })
+
+        const headerSearchClose = document.getElementById(
+            `${this.selector}-search-close`
+        )
+        if (!headerSearchClose && !headerSearchMobile) return
+        headerSearchClose.addEventListener('click', () => {
+            headerSearchMobile.classList.remove('active')
+            document.querySelector('body').classList.remove('o-hidden')
         })
     }
 }
